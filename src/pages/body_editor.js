2adf69d0c7cb0b390843f6308c1ff3513bf19405
ffitstone <dreamersfly@163.com>
2019-07-25 19:13:30 +0800

    🚀  feature(layout): layout 添加删除和移动的实现

--- src/pages/body_editor.js
+++ src/pages/body_editor.js
@@ -24,6 +24,8 @@ class Page {
         this.layout_data = observable([]);
         this.add_layout_group_dialog = {};
         this.delete_layout_group_dialog = {};
+        this.add_layout_dialog = {};
+        this.delete_layout_dialog = {};
     }
 
     init() {
@@ -39,14 +41,6 @@ class Page {
         });
     }
     get_layout_group_data(col) {
-        let cols_dom = String(col)
-            .split("_")
-            .map(v => {
-                return {
-                    col: v,
-                    dom: "<p>编辑器</p>"
-                };
-            });
         return {
             id: stringRandom(16, { numbers: false }),
             dom: null,
@@ -70,21 +64,31 @@ class Page {
                     repeat: false,
                     parallax: false,
                     align_center: false
-                },
-                body: [
-                    {
-                        animate: 0,
-                        x_align: false,
-                        y_align: false,
-                        body_dom: null,
-                        col: col || "100",
-                        padding_x: "",
-                        padding_y: "",
-                        backgroundColor: [],
-                        col_container: cols_dom
-                    }
-                ]
-            }
+                }
+            },
+            body: [this.get_layout_data(col)]
+        };
+    }
+    get_layout_data(col) {
+        let cols_dom = String(col)
+            .split("_")
+            .map(v => {
+                return {
+                    col: v,
+                    dom: "<p>编辑器</p>"
+                };
+            });
+        return {
+            id: stringRandom(16, { numbers: false }),
+            animate: 0,
+            x_align: false,
+            y_align: false,
+            body_dom: null,
+            col: col || "100",
+            padding_x: "",
+            padding_y: "",
+            backgroundColor: [],
+            col_container: cols_dom
         };
     }
     add_layout_group(col, inert_id) {
@@ -99,11 +103,29 @@ class Page {
             this.layout_data.push(layout_group_data);
         }
     }
+    add_layout(col, group_id, layout_id) {
+        this.search_layout_group_data(group_id).body.splice(
+            this.search_layout_index(group_id, layout_id),
+            0,
+            this.get_layout_data(col)
+        );
+    }
     search_layout_group_index(id) {
         let result = NaN;
         this.layout_data.forEach((v, i) => {
-            console.log(v);
             if (v.id && v.id == id) {
+                result = i;
+            }
+        });
+        return result;
+    }
+    search_layout_group_data(id) {
+        return this.layout_data[this.search_layout_group_index(id)];
+    }
+    search_layout_index(group_id, layout_id) {
+        let result = NaN;
+        this.search_layout_group_data(group_id).body.forEach((v, i) => {
+            if (v.id && v.id == layout_id) {
                 result = i;
             }
         });
@@ -113,34 +135,44 @@ class Page {
         if (typeof String(id) != "string") return;
         this.layout_data.splice(this.search_layout_group_index(id), 1);
     }
-    create_layout_group_dialog(config) {
+    delete_layout(group_id, layout_id) {
+        if (!group_id || !layout_id) return;
+        this.search_layout_group_data(group_id).body.splice(
+            this.search_layout_index(group_id, layout_id),
+            1
+        );
+    }
+    create_dialog(config) {
         return dialog(Object.assign({}, config)).init();
     }
-    layout_group_move_down(data) {
+    layout_group_move(data, dir) {
         if (!data.id) return this;
-        let layout_group_id = this.search_layout_group_index(data.id);
-        [
-            this.layout_data[layout_group_id],
-            this.layout_data[layout_group_id + 1]
-        ] = [
-            this.layout_data[layout_group_id + 1],
-            this.layout_data[layout_group_id]
+        let layout_group_index = this.search_layout_group_index(data.id);
+        let target =
+            dir == "up" ? layout_group_index - 1 : layout_group_index + 1;
+        [this.layout_data[layout_group_index], this.layout_data[target]] = [
+            this.layout_data[target],
+            this.layout_data[layout_group_index]
         ];
     }
-    layout_group_move_up(data) {
-        if (!data.id) return this;
-        let layout_group_id = this.search_layout_group_index(data.id);
-        [
-            this.layout_data[layout_group_id - 1],
-            this.layout_data[layout_group_id]
-        ] = [
-            this.layout_data[layout_group_id],
-            this.layout_data[layout_group_id - 1]
+    layout_move(
+        { group_id, layout_id } = { group_id: NaN, layout_id: NaN },
+        dir
+    ) {
+        if (!group_id || !layout_id) return this;
+
+        let target_layout_index = this.search_layout_index(group_id, layout_id);
+        let group_data = this.search_layout_group_data(group_id);
+        let target =
+            dir == "up" ? target_layout_index - 1 : target_layout_index + 1;
+        [group_data.body[target_layout_index], group_data.body[target]] = [
+            group_data.body[target],
+            group_data.body[target_layout_index]
         ];
     }
     set delete_layout_group_dialog(config) {
         const Self = this;
-        this._delete_layout_dialog = this.create_layout_group_dialog(
+        this._delete_layout_group_dialog = this.create_dialog(
             Object.assign(
                 {
                     type: "warn",
@@ -149,8 +181,14 @@ class Page {
                     dialog_header: "删除布局组",
                     dialog_body: `<p style="text-align: center">是否确认删除当前布局容器</p>`,
                     dialog_footer: "",
-                    confirm_ev(done, data) {
-                        Self.delete_layout_group(data.id);
+                    confirm_ev(
+                        done,
+                        { group_id, layout_id } = {
+                            group_id: NaN,
+                            layout_id: NaN
+                        }
+                    ) {
+                        Self.delete_layout_group(group_id, layout_id);
                         done();
                     },
                     cancel_ev() {}
@@ -160,27 +198,57 @@ class Page {
         );
     }
     get delete_layout_group_dialog() {
+        return this._delete_layout_group_dialog;
+    }
+    set delete_layout_dialog(config) {
+        const Self = this;
+        this._delete_layout_dialog = this.create_dialog(
+            Object.assign(
+                {
+                    type: "warn",
+                    box_size: "big",
+                    dialog_style: "width: 300px;font-size: 16px;",
+                    dialog_header: "删除布局",
+                    dialog_body: `<p style="text-align: center">是否确认删除当前布局</p>`,
+                    dialog_footer: "",
+                    confirm_ev(
+                        done,
+                        { group_id, layout_id } = {
+                            group_id: NaN,
+                            layout_id: NaN
+                        }
+                    ) {
+                        Self.delete_layout(group_id, layout_id);
+                        done();
+                    },
+                    cancel_ev() {}
+                },
+                config
+            )
+        );
+    }
+    get delete_layout_dialog() {
         return this._delete_layout_dialog;
     }
     set add_layout_group_dialog(config) {
         const Self = this;
-        this._add_layout_group_dialog = this.create_layout_group_dialog(
+        this._add_layout_group_dialog = this.create_dialog(
             Object.assign(
                 {
-                    dom: $(".page_editor-layout_pop"),
+                    dom: $(".add-layout_group_pop"),
 
                     box_size: "big",
                     dialog_header: "添加布局容器",
                     dialog_body: null,
                     dialog_footer: "",
-                    confirm_ev(done, data) {
+                    confirm_ev(done, { id } = { id: NaN }) {
                         Self.add_layout_group(
                             this.$container
                                 .find(
                                     ".page_editor-layout_options .item.active"
                                 )
                                 .data("value"),
-                            data && data.id
+                            id
                         );
                         done();
                     },
@@ -195,6 +263,47 @@ class Page {
 
     get add_layout_group_dialog() {
         return this._add_layout_group_dialog;
+    }
+
+    set add_layout_dialog(config) {
+        const Self = this;
+        this._add_layout_dialog = this.create_dialog(
+            Object.assign(
+                {
+                    dom: $(".add-layout_pop"),
+                    box_size: "big",
+                    dialog_header: "添加布局",
+                    dialog_body: null,
+                    dialog_footer: "",
+                    confirm_ev(
+                        done,
+                        { group_id, layout_id } = {
+                            group_id: NaN,
+                            layout_id: NaN
+                        }
+                    ) {
+                        Self.add_layout(
+                            this.$container
+                                .find(
+                                    ".page_editor-layout_options .item.active"
+                                )
+                                .data("value"),
+                            group_id,
+                            layout_id
+                        );
+                        done();
+                    },
+                    cancel_ev() {
+                        alert("concel");
+                    }
+                },
+                config
+            )
+        );
+    }
+
+    get add_layout_dialog() {
+        return this._add_layout_dialog;
     }
 }
 
@@ -228,17 +337,51 @@ Page.init_dos.push(function initlayoutGroupBar() {
                     break;
 
                 case "down":
-                    PageClass.layout_group_move_down({ id: id });
+                    PageClass.layout_group_move({ id: id }, "down");
                     break;
 
                 case "up":
-                    PageClass.layout_group_move_up({ id: id });
+                    PageClass.layout_group_move({ id: id }, "up");
                     break;
                 default:
                     break;
             }
         }
     );
+    PageClass.$container.on("click", ".layout-editor_bar .item", function() {
+        let key = $(this).data("key");
+        let group_id = $(this)
+            .closest(".layout_group")
+            .attr("id");
+
+        let layout_id = $(this)
+            .closest(".layout")
+            .attr("id");
+
+        switch (key) {
+            case "editor":
+                // PageClass.delete_layout_dialog.show({ group_id, layout_id });
+                alert("editor");
+                break;
+            case "delete":
+                PageClass.delete_layout_dialog.show({ group_id, layout_id });
+                break;
+
+            case "add":
+                PageClass.add_layout_dialog.show({ group_id, layout_id });
+                break;
+
+            case "down":
+                PageClass.layout_move({ group_id, layout_id }, "down");
+                break;
+
+            case "up":
+                PageClass.layout_move({ group_id, layout_id }, "up");
+                break;
+            default:
+                break;
+        }
+    });
 });
 Page.init_dos.push(function layoutSelect() {
     $(".page_editor-layout_options").on("click", ".item", function() {
