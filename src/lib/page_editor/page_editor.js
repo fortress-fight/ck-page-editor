dc0dfd8010ebfe3d569403ae90ab91db7055a14f
ffitstone <dreamersfly@163.com>
2019-08-06 11:30:48 +0800

    🚀  feature(tool_bar): 添加预览按钮

--- src/lib/page_editor/page_editor.js
+++ src/lib/page_editor/page_editor.js
@@ -34,24 +34,38 @@ class Page_editor {
         this.container.html(this.editor_dom);
         setTimeout(() => {
             this.set_data();
+            this.set_view_oper();
             this.init_done();
         });
         return this;
     }
 
     set_data() {
-        this.editor_iframe[0].contentWindow.frame_data = this.editor_frame_data;
+        try {
+            this.editor_iframe[0].contentWindow.set_data(
+                this.editor_frame_data
+            );
+        } catch (error) {
+            console.log("缺少正确的内部框架");
+        }
     }
 
     get_data() {
-        return this.editor_iframe[0].contentWindow.data;
+        return this.editor_iframe[0].contentWindow.get_data();
     }
-
+    set_view_oper() {
+        let _this = this;
+        this.$toolsbar.on("click", ".view_btn .theme .btn", function() {
+            _this.theme = $(this).attr("data-value");
+        });
+        this.$toolsbar.on("click", ".view_btn .agent .btn", function() {
+            _this.agent = $(this).attr("data-value");
+        });
+    }
     fool_screen(control) {
         switch (control) {
             case "open":
                 this.$editor_dom.addClass("page_editor-fool_screen");
-
                 break;
             case "close":
                 this.$editor_dom.removeClass("page_editor-fool_screen");
@@ -109,7 +123,28 @@ class Page_editor {
             }
         );
     }
-
+    get theme() {
+        return value;
+    }
+    set theme(value) {
+        if (value == this._theme) return;
+        this._theme = value;
+        this.$toolsbar.find(".theme .btn").removeClass("active");
+        this.$toolsbar
+            .find('.theme .btn[data-value="' + value + '"]')
+            .addClass("active");
+    }
+    get agent() {
+        return value;
+    }
+    set agent(value) {
+        if (value == this._agent) return;
+        this._agent = value;
+        this.$toolsbar.find(".agent .btn").removeClass("active");
+        this.$toolsbar
+            .find('.agent .btn[data-value="' + value + '"]')
+            .addClass("active");
+    }
     get fool_screen_btn() {
         return {
             name: "fool_screen",
@@ -229,7 +264,7 @@ class Page_editor {
         this.tools_option.forEach(tool => {
             result += get_btn_dom(tool);
         });
-        result += "</div></div>";
+        result += `</div><div class="view_btn"> <div class="theme"> <span class="theme_white-btn btn active" data-value="white"></span> <span class="theme_black-btn btn" data-value="black"></span> </div> <div class="line"></div> <div class="agent"> <span class="agent_pc-btn btn active fa fa-MacBookPro" data-value="pc"> </span> <span class="agent_mo-btn btn fa fa-iphone" data-value="mo"> </span> </div> </div></div>`;
         return result;
     }
 
